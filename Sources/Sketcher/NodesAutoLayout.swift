7b8769239354bc15d13f474a8fc71ade45524f42
Raheel Ahmad <ahmadraheel@gmail.com>
2021-04-09 22:03:46 -0700

    begin autolayout

--- Sources/Sketcher/NodesAutoLayout.swift
+++ Sources/Sketcher/NodesAutoLayout.swift
@@ -1,6 +1,27 @@
 import CoreGraphics
 import NodeView
+import Models
+
+protocol LayoutNode {
+    var id: String { get }
+    var childNodeIds: Set<String> { get }
+    var fractPos: Position { get set }
+}
+
+extension Node: LayoutNode {
+    var childNodeIds: Set<String> { linkedNodeIds }
+}
 
 class NodesAutoLayout {
+    struct Metrics {
+        let nodeSize: CGSize
+        let nodeSpacingX: CGFloat
+        let nodeSpacingY: CGFloat
 
+        let interSiblingsSpacing: CGFloat
+
+        let rowSpacing: CGFloat
+
+    }
+    static func layout(nodes: [LayoutNode])
 }
