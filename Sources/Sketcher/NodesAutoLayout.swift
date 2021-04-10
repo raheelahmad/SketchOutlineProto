e71517ba693d2b5ab0c3845783a550cc17261be5
Raheel Ahmad <ahmadraheel@gmail.com>
2021-04-10 14:11:27 -0700

    building nodes layout tree

--- Sources/Sketcher/NodesAutoLayout.swift
+++ Sources/Sketcher/NodesAutoLayout.swift
@@ -12,6 +12,15 @@ extension Node: LayoutNode {
     var childNodeIds: Set<String> { linkedNodeIds }
 }
 
+struct LayoutNodeSiblings: CustomDebugStringConvertible {
+    let parentId: String?
+    let siblings: [LayoutNode]
+
+    var debugDescription: String {
+        "parentId: \(parentId ?? ""), siblingIds: \(siblings.map {$0.id}.joined(separator: " "))"
+    }
+}
+
 class NodesAutoLayout {
     struct Metrics {
         let nodeSize: CGSize
@@ -23,5 +32,33 @@ class NodesAutoLayout {
         let rowSpacing: CGFloat
 
     }
-    static func layout(nodes: [LayoutNode])
+
+    static func layout(nodes: [LayoutNode], m: Metrics) {
+
+    }
+
+    static func tree(from nodes: [LayoutNode]) -> [[LayoutNodeSiblings]] {
+        func findChildren(node: LayoutNode) -> LayoutNodeSiblings {
+            let children = nodes.filter { node.childNodeIds.contains($0.id) }
+            return LayoutNodeSiblings(parentId: node.id, siblings: children)
+        }
+
+        var rootNodes = nodes.filter { node in
+            !nodes.contains(where: { $0.childNodeIds.contains(node.id) })
+        }
+        var rootNodeSiblings = rootNodes.map {
+            [LayoutNodeSiblings(parentId: nil, siblings: [$0])]
+        }
+
+        while !rootNodes.isEmpty {
+            let siblings = rootNodes.map { findChildren(node: $0) }
+            rootNodeSiblings.append(siblings)
+            rootNodes = siblings
+                .flatMap { $0.siblings
+                    .filter { node in !node.childNodeIds.isEmpty } // only include non-leaf nodes as next root node
+                }
+        }
+
+        return rootNodeSiblings
+    }
 }
--- Tests/SketcherTests/AutoLayoutTests.swift
+++ Tests/SketcherTests/AutoLayoutTests.swift
@@ -1,16 +1,98 @@
 import XCTest
+import Models
 @testable import Sketcher
 
+extension Position {
+    static var zero: Position { .init(x: 0, y: 0) }
+}
+
 final class AutoLayoutTests: XCTestCase {
-    func testExample() {
-        let point = CGRect.linkPoints(
-            from: CGRect(x: 0, y: 0, width: 200, height: 300),
-            to: CGRect(x: 0, y: 0, width: 200, height: 300)
+    func testAutoLayout() {
+        XCTAssertEqual(2, 2)
+    }
+
+
+    func testTreeStructure() {
+        struct AssertProps {
+            let parentId: String?
+            let siblingIds: Set<String>
+        }
+
+        func assert(props: [AssertProps], forRow row: Int, rowSiblings: [LayoutNodeSiblings]) {
+            XCTAssertEqual(rowSiblings.count, props.count, "Should have correct number of sibling groups in row \(row)")
+            for (idx, prop) in props.enumerated() {
+                let siblings = rowSiblings[idx]
+                XCTAssertEqual(siblings.parentId, prop.parentId, "Should have correct parentId for row \(row)")
+                XCTAssertEqual(siblings.siblings.count, prop.siblingIds.count, "Should have correct number of siblings")
+                let ids = siblings.siblings.map { $0.id }
+                for expectedId in prop.siblingIds {
+                    XCTAssert(ids.contains(expectedId), "Should have the expected sibling")
+                }
+            }
+        }
+
+        let nodesTree = NodesAutoLayout.tree(from: nodes())
+        XCTAssertEqual(nodesTree.count, 4)
+
+        let firstRow = nodesTree[0]
+        assert(props: [.init(parentId: nil, siblingIds: ["a"])], forRow: 0, rowSiblings: firstRow)
+
+        let secondRow = nodesTree[1]
+        assert(
+            props: [
+                .init(parentId: "a", siblingIds: ["b", "c"]),
+            ],
+            forRow: 1,
+            rowSiblings: secondRow
         )
-        XCTAssertEqual(point.0, point.1)
+
+        let thirdRow = nodesTree[2]
+        assert(
+            props: [
+                .init(parentId: "b", siblingIds: ["d"]),
+                .init(parentId: "c", siblingIds: ["e", "f"]),
+            ],
+            forRow: 2,
+            rowSiblings: thirdRow
+        )
+
+        let fourthRow = nodesTree[3]
+        assert(
+            props: [
+                .init(parentId: "d", siblingIds: ["g", "h", "i"]),
+                .init(parentId: "e", siblingIds: ["j", "k"]),
+                .init(parentId: "f", siblingIds: ["l", "m"]),
+            ],
+            forRow: 3,
+            rowSiblings: fourthRow
+        )
+    }
+
+    struct Node: LayoutNode {
+        let id: String
+        let childNodeIds: Set<String>
+        var fractPos: Position
+    }
+
+    private func nodes() -> [Node] {
+        [
+            Node(id: "a", childNodeIds: ["b", "c"], fractPos: .zero),
+            Node(id: "b", childNodeIds: ["d",], fractPos: .zero),
+            Node(id: "c", childNodeIds: ["e", "f"], fractPos: .zero),
+            Node(id: "d", childNodeIds: ["g", "h", "i"], fractPos: .zero),
+            Node(id: "e", childNodeIds: ["j", "k", ], fractPos: .zero),
+            Node(id: "f", childNodeIds: ["l", "m", ], fractPos: .zero),
+            Node(id: "g", childNodeIds: [], fractPos: .zero),
+            Node(id: "h", childNodeIds: [], fractPos: .zero),
+            Node(id: "i", childNodeIds: [], fractPos: .zero),
+            Node(id: "j", childNodeIds: [], fractPos: .zero),
+            Node(id: "k", childNodeIds: [], fractPos: .zero),
+            Node(id: "l", childNodeIds: [], fractPos: .zero),
+            Node(id: "m", childNodeIds: [], fractPos: .zero),
+        ]
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testAutoLayout", testAutoLayout),
     ]
 }
